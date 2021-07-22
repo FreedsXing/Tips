ea6427f2cd47cdbcd83e16de19a59d38e885df33
xinghl <xinghl@zfxf.com>
2021-07-22 16:44:50 +0800

    添加登录功能

--- app/src/main/java/com/help/tip/MainActivity.java
+++ app/src/main/java/com/help/tip/MainActivity.java
@@ -2,16 +2,59 @@ package com.help.tip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * @author Administrator
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.et_account)
+    EditText etAccount;
+    @BindView(R.id.et_pwd)
+    EditText etPwd;
+    @BindView(R.id.tv_login)
+    TextView tvLogin;
+
+    @BindView(R.id.tv_login_fingerprint)
+    TextView tvLoginFingerPrint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        tvLogin.setOnClickListener(this);
+        tvLoginFingerPrint.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_login:
+                String accout = etAccount.getText().toString();
+                String pwd = etPwd.getText().toString();
+                if ("123".equals(accout) && "456".equals(pwd)){
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(MainActivity.this, "账号或密码失败", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.tv_login_fingerprint:
+
+                break;
+            default:
+                break;
+        }
     }
 }
