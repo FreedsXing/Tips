15ec5326fc169c09b79d124cbbcb2d30b07a6d7e
xinghl <xinghl@zfxf.com>
2021-07-23 16:08:00 +0800

    HomeActivity 添加子Fragment

--- app/src/main/java/com/help/tip/HomeActivity.java
+++ app/src/main/java/com/help/tip/HomeActivity.java
@@ -1,14 +1,66 @@
 package com.help.tip;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import butterknife.ButterKnife;
 
 public class HomeActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        frameLayout = findViewById(R.id.framelayout);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.framelayout, new HomeFragment());
+        transaction.commitAllowingStateLoss();
+
+
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                switch (item.getItemId()){
+                    case R.id.first:
+                        transaction.replace(R.id.framelayout, new HomeFragment());
+                        break;
+                    case R.id.second:
+                        transaction.replace(R.id.framelayout, new SecondFragment());
+                        break;
+                    case R.id.third:
+                        transaction.replace(R.id.framelayout, new ThirdFragment());
+                        break;
+                    case R.id.four:
+                        transaction.replace(R.id.framelayout, new FourthFragment());
+                        break;
+                    case R.id.five:
+                        transaction.replace(R.id.framelayout, new FifthFragment());
+                        break;
+                    default:
+                        break;
+                }
+                transaction.commitAllowingStateLoss();
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/com/help/tip/MainActivity.java
+++ app/src/main/java/com/help/tip/MainActivity.java
@@ -70,7 +70,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
