49db6da52829563a3513089e4748ef1e0fa8fdfa
xinghl <xinghl@zfxf.com>
2021-08-10 15:21:43 +0800

    提取ak

---
+++ app/src/main/java/com/help/tips/base/Constants.java
@@ -0,0 +1,11 @@
+package com.help.tips.base;
+
+public class Constants {
+
+
+    /**
+     * 定位SDK自V4.0版本之后，引入了百度地图开放平台的统一AK验证体系
+     */
+    public static final String BAIDU_MAP_API_KEY = "";
+
+}
